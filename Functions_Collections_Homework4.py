2a0ef3f89bffffbe0483adadc251a44ae30d25ec
Nadiia Radionenko <Nadiia_Radionenko@epam.com>
2021-02-05 12:09:41 +0200

    Fixed a bug: previously committed version was throwing an error

--- Functions_Collections_Homework4.py
+++ Functions_Collections_Homework4.py
@@ -7,23 +7,30 @@ def values_to_list(my_list):
     flat_dict = {}
     for dct in my_list:
         for k in dct.keys():
-            values_lst = [d[k] for d in my_list if k in dct.keys()]
-            flat_dict.update(dict.fromkeys(k, values_lst))
+            values_lst = [d[k] for d in my_list if k in d.keys()]
+            new_dict = dict.fromkeys(k, values_lst)
+            flat_dict.update(new_dict)
     return flat_dict
 
 
-# select max value for each key and its occurrence; update key name and add it with max key to final dictionary
-def select_max_rename(my_dict):
+# find max value for each key and its occurrence
+def find_max_value(my_dict, my_key):
+    max_value, max_occurrence = my_dict[my_key][0], 1
+    for i in range(len(my_dict[my_key])):
+            if my_dict[my_key][i] > max_value:
+                max_value = my_dict[my_key][i]
+                max_occurrence = i + 1
+    return max_value, max_occurrence
+
+
+# put together final dict with max values for each key and renaming the key based on occurrence
+def create_new_dict_and_rename(my_dict):
     final_dict = {}
     for key, values_list in my_dict.items():
         if len(values_list) == 1:
             final_dict[key] = values_list[0]
         else:
-            max_value, max_position = values_list[0], 1
-            for i in range(len(values_list)):
-                if values_list[i] > max_value:
-                    max_value = values_list[i]
-                    max_position = i + 1
+            max_value, max_position = find_max_value(my_dict, key)
             final_dict[key + '_' + str(max_position)] = max_value
     print(final_dict)
 
@@ -32,4 +39,4 @@ rand_lst = [{choice(ascii_lowercase): randint(0,100) for i in range(len(ascii_lo
 
 if __name__ == '__main__':
     temp_dict = values_to_list(rand_lst)
-    select_max_rename(temp_dict)
+    create_new_dict_and_rename(temp_dict)
