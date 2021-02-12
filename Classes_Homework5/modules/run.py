0760e169b3299662217dd7c012c2d4994ee91fc4
Nadiia Radionenko <Nadiia_Radionenko@epam.com>
2021-02-12 21:01:04 +0200

    Classes homework + Functions collections fixed

---
+++ Classes_Homework5/modules/run.py
@@ -0,0 +1,37 @@
+from exec_utils.configloader import Config
+from modules.input import Selection
+from modules.file import Files
+from modules.values import Values
+
+cnf = Config()
+s = Selection()
+f = Files(cnf.get_values("PATHS", "target_file"))
+
+label1, label2, label3 = cnf.get_values("LABELS", "news_label"), cnf.get_values("LABELS", "ad_label"), cnf.get_values("LABELS", "recipe_label")
+decor, decor_length = cnf.get_values("RESTRICTIONS", "n"), cnf.get_values("RESTRICTIONS", "count_n")
+
+
+class Execute:
+    def prepare_for_file(self):
+        """Ask user for selection and then ask for all further details based on selected value.
+        Combine input values into 1 section (=prepare for writing to the file)
+        """
+        selection = s.ask_for_selection(label1, label2, label3)
+        val = Values(selection, decor, decor_length)
+        if selection == label1:
+            news = val.get_news()
+            return news
+        elif selection == label2:
+            ad = val.get_ad()
+            return ad
+        elif selection == label3:
+            recipe = val.get_recipe()
+            return recipe
+
+    def end_to_end(self):
+        """Get formatted and prepared for write input.
+        Write to the file.
+        File name is configurable
+        """
+        text_for_file = self.prepare_for_file()
+        f.write_file(text_for_file)
--- Functions_Collections_Homework4.py
+++ Functions_Collections_Homework4.py
@@ -2,8 +2,18 @@ from string import ascii_lowercase  # we will use it to generate keys as letters
 from random import randint, choice  # we will use it to generate random integers for dictionary values and list length; choice will be used to select random letter for keys
 
 
-# create new dicts where key = key from random list and value = list of values for this key
+def rand_list(min_int, max_int, min_count, max_count):
+    """Generate list of dictionaries, where:
+    dict key = random letter
+    dict value = random integer from min_int to max_int
+    number of list elements is random value from min_count to max_count
+    """
+    rand_lst = [{choice(ascii_lowercase): randint(min_int, max_int) for i in range(len(ascii_lowercase))} for j in range(randint(min_count, max_count))]
+    return rand_lst
+
+
 def values_to_list(my_list):
+    """Create new dict where key = key from random list and value = list of values for this key"""
     flat_dict = {}
     for dct in my_list:
         for k in dct.keys():
@@ -13,30 +23,29 @@ def values_to_list(my_list):
     return flat_dict
 
 
-# find max value for each key and its occurrence
-def find_max_value(my_dict, my_key):
-    max_value, max_occurrence = my_dict[my_key][0], 1
-    for i in range(len(my_dict[my_key])):
-            if my_dict[my_key][i] > max_value:
-                max_value = my_dict[my_key][i]
-                max_occurrence = i + 1
+def find_max_value(my_list):
+    """Find max value and its occurrence within the list"""
+    max_value, max_occurrence = my_list[0], 1
+    for i in range(len(my_list)):
+        if my_list[i] > max_value:
+            max_value = my_list[i]
+            max_occurrence = i + 1
     return max_value, max_occurrence
 
 
-# put together final dict with max values for each key and renaming the key based on occurrence
 def create_new_dict_and_rename(my_dict):
+    """Put together final dict with max values for each key and renaming the key based on occurrence"""
     final_dict = {}
     for key, values_list in my_dict.items():
         if len(values_list) == 1:
             final_dict[key] = values_list[0]
         else:
-            max_value, max_position = find_max_value(my_dict, key)
+            max_value, max_position = find_max_value(values_list)
             final_dict[key + '_' + str(max_position)] = max_value
     print(final_dict)
 
 
-rand_lst = [{choice(ascii_lowercase): randint(0,100) for i in range(len(ascii_lowercase))} for j in range(randint(2,10))]  # list of random dicts
-
 if __name__ == '__main__':
+    rand_lst = rand_list(0,100, 2, 10)
     temp_dict = values_to_list(rand_lst)
     create_new_dict_and_rename(temp_dict)
