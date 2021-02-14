47019e005e8036b633801161250ce2fa132972f5
Nadiia Radionenko <Nadiia_Radionenko@epam.com>
2021-02-14 18:14:57 +0200

    Latest changes to Classes + Updated Strings functions homework

--- Functions_Strings_Homework4.py
+++ Functions_Strings_Homework4.py
@@ -1,42 +1,56 @@
 import re
 
 
-# make sentence from the last words
-def collect_last_words(strng):
-    last_elements = ' '.join(re.findall(r'\w+(?=[.?!])', strng)) + '.'  # find all words in text followed by ending characters (.?!) and join them into sentence
-    return last_elements
-
-
-# add last words sentence to the text. Identify where to position new sentence by 'after words' parameter
-def add_new_sentence(strng, after_words, new_words):
+def make_new_sentence(strng, split_pattern, n):
+    """Split string by pattern into substrings. Split each substring by spaces into words. Make new sentence from n word from each sub_string.
+       n = 0 for 1st element, n = -1 for last element.
+    """
     try:
-        position = re.search(after_words, strng.lower()).end() + 1  # search string position where new sentence should start
-        new_string = strng[:position] + ' ' + new_words + strng[position:]  # split string into "before" and "after" position and insert new sentence between them
-        return new_string
-    except AttributeError:  # if 'after words' can't be found in the string, print an error message
-        print("ERROR ENCOUNTERED: Can't identify the place where to insert new sentence. New sentence not inserted. \n\n\n")
-        return strng
+        sentences = re.split(split_pattern, strng)
+        temp_words = [sentences[i].split()[n] for i in range(len(sentences)-1)]
+        words = [re.sub('\W', "", word) for word in temp_words]
+        new_sentence  = ' '.join(words) + '.'
+        return new_sentence
+    except IndexError:
+        print("No such element in sentence. New sentence can't be created.\n")
+        return ""
 
 
-# fix spelling: replace "iz" with "is" whe is is a separate word not surrounded by any non-whitespace characters
+def add_new_sentence(strng, split_pattern, position, new_words):
+    """Split string by paragraphs.
+    Add new sentence to the specified paragraph sequence(starting from 1)
+    """
+    lst = re.split(split_pattern, strng)
+    if position > len(lst) or position < 1:
+        print(f"Paragraph with defined position doesn't exist. Min position = 1, max position = {len(lst)}\n")
+        str = strng
+    else:
+        output = []
+        for i in range(len(lst)):
+            if i == position - 1:  # position == paragraph sequence number
+                output.append(lst[i].rstrip() + ' ' + f"{new_words}\n\n")
+            else:
+                output.append(lst[i])
+        str = split_pattern.join(output)
+    return str
+
+
 def fix_spelling(strng, check_str, replace_str):
-    fixed_str = re.sub(rf'(?<!\S)(\b{check_str}\b)(?!\S)', replace_str, strng, flags = re.I)  # find "iz" not surrounded by any nonwhitespace chars and replzce it with "is"
+    """Fix spelling: replace "iz" with "is" whe is is a separate word not surrounded by any non-whitespace characters"""
+    fixed_str = re.sub(rf'(?<!\S)(\b{check_str}\b)(?!\S)', replace_str, strng, flags = re.I)
     return fixed_str
 
 
-# capitalize first words after given pattern:
-def set_to_capital(m):
-    return m.group(1) + m.group(2).capitalize()  # where group 1 is going to be whitespaces before first word and group 2 - first word in each sentence or new paragraph
+def capitalize(strng, pattern):
+    """Split string by pattern (in our case - ending chars + space or /n/n+space).
+    Capitalize each element in split list and join back together into string.
+    """
+    temp_lst= re.split(pattern, strng)
+    capitalized_lst = [i.capitalize() for i in temp_lst]
+    final_str = ''.join(capitalized_lst)
+    return final_str
 
 
-# normalize text
-def normalize(strng):
-    pattern = re.compile(r"(^|[?!.]\s+|\n\s+)(\S+)")  # first word in string OR word preceded by ending char and whitespaces OR word preceded by newline and whitespace (to capture new paragraph)
-    normalized = re.sub(pattern, set_to_capital, strng.lower())  # set text to lower first and then capitalize first words
-    return normalized
-
-
-# count whitespaces
 def count_whitespaces (strng):
     cnt = 0
     for i in strng:
@@ -60,11 +74,15 @@ original_str = '''homEwork:
 
 
   last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87.'''
+sentence_pattern = '[?!.]'
+paragraph_pattern = '\n\n '
+capitalize_pattern = '([?!.][\s]|\s{2,})'  # brackets allow to split and keep split delimiters at the same time. This pattern reads as: ending characters+whitespace OR at least 2 whitespaces (for paragraphs)
+
 
 if __name__ == '__main__':
-    last_words = collect_last_words(original_str)
-    string_with_new_sentence = add_new_sentence(original_str, 'add it to the end of this paragraph', last_words)
+    last_words = make_new_sentence(original_str, sentence_pattern, -1)
+    string_with_new_sentence = add_new_sentence(original_str, paragraph_pattern, 3, last_words)
     fixed_spelling = fix_spelling(string_with_new_sentence, "iz", "is")
-    final_string = normalize(fixed_spelling)
+    final_string = capitalize(fixed_spelling, capitalize_pattern)
     print(final_string)
     count_whitespaces(final_string)
